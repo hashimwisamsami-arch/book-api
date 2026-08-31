21748d6627f90d16818bf6f2ddff8f87059c25c2
Hashim <hashimwisamsami@email.com>
2026-08-31 13:03:30 +0300

    feat: implement user model with validation functions

---
+++ models/User.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 100,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 200,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", UserSchema);
+
+//Validate Register User
+function validateRegisterUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).email().required(),
+    username: Joi.string().trim().min(2).max(200).required(),
+    password: Joi.string().trim().min(6).required(),
+    isAdmin: Joi.boolean(),
+  });
+  return schema.validate(obj);
+}
+
+//Validate Login User
+function validateLoginUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).email().required(),
+    password: Joi.string().trim().min(6).required(),
+  });
+  return schema.validate(obj);
+}
+
+//Validate Update User
+function validateUpdateUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).email(),
+    username: Joi.string().trim().min(2).max(200),
+    password: Joi.string().trim().min(6),
+    isAdmin: Joi.boolean(),
+  });
+  return schema.validate(obj);
+}
+
+module.exports = {
+  User,
+  validateRegisterUser,
+  validateLoginUser,
+  validateUpdateUser,
+};

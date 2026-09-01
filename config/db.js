e95c3b1d689c042d8f9985f651d69269aad996d2
Hashim <hashimwisamsami@email.com>
2026-09-01 13:33:08 +0300

    fix: make database connection function asynchronous

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-function connectToDB() {
+async function connectToDB() {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected To MongoDB...");
   } catch (error) {
     console.log("Connection failed to MongoDB!", error);

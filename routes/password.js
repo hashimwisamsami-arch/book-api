4c49caafd9ad621c7331c828c185fcbe477db863
Hashim <hashimwisamsami@email.com>
2026-09-02 15:22:50 +0300

    feat: implement reset password functionality with views, routes, and email handling

--- routes/password.js
+++ routes/password.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const { getForgotPasswordView } = require("../controllers/passwordController");
+const {
+  getForgotPasswordView,
+  sendForgotPasswordLink,
+  GetResetPasswordView,
+  resetThePassword,
+} = require("../controllers/passwordController");
 const router = express.Router();
 
 // /password/forgot-password
-router.route("/forgot-password").get(getForgotPasswordView);
+router
+  .route("/forgot-password")
+  .get(getForgotPasswordView)
+  .post(sendForgotPasswordLink);
+
+// password/reset-password/:userId/:token
+router
+  .route("/reset-password/:userId/:token")
+  .get(GetResetPasswordView)
+  .post(resetThePassword);
 module.exports = router;

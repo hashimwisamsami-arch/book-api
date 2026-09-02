e287c6793b4d3952ff43dec77ef66ca8d65ea51a
Hashim <hashimwisamsami@email.com>
2026-09-02 17:08:16 +0300

    feat: add image upload functionality with multer and create upload route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectToDB = require("./config/db");
 require("dotenv").config();
+const path = require("path");
+
 const logger = require("./middlewares/logger");
 const { notFound, errorHandler } = require("./middlewares/errors");
 
@@ -8,6 +10,9 @@ const { notFound, errorHandler } = require("./middlewares/errors");
 connectToDB();
 //Init App
 const app = express();
+
+//Static folder
+app.use(express.static(path.join(__dirname, "images")));
 
 //Applay Middlewares
 app.use(express.json());
@@ -21,6 +26,7 @@ app.use("/api/books", require("./routes/books"));
 app.use("/api/authors", require("./routes/authors"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
+app.use("/api/upload", require("./routes/upload"));
 app.use("/password", require("./routes/password"));
 
 //Error Handler
